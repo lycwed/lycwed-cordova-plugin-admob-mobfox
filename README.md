acedcfe1bf404d0cba6d1c88b7650a6defac4cb9
lycwed dev <ylechevalier@gmail.com>
2018-01-23 23:11:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Cordova AdMob Mediation Plugin for MobFox
+
+Plugin compatible with cordova-plugin-admob-free
+
+## Installation
+
+```sh
+cordova plugin add lycwed-cordova-admob-plugin-mobfox --save
+```
---
+++ src/ios/GADMAdapterMobFox.h
@@ -0,0 +1,7 @@
+
+#import <MobFoxSDKCore/MobFoxSDKCore.h>
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+@interface GADMAdapterMobFox : NSObject <GADMAdNetworkAdapter, MobFoxAdTagDelegate, MobFoxTagInterstitialAdDelegate>
+
+@end
